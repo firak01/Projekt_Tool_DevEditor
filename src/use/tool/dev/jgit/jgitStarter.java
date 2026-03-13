f27185e52112575ac26324505191047e59d1406b
firak01 <firak01@t-online.de>
2026-03-13 11:09:56 +0100

    in Arbeit Signed-off-by: firak01 <firak01@t-online.de>

--- src/use/tool/dev/jgit/jgitStarter.java
+++ src/use/tool/dev/jgit/jgitStarter.java
@@ -8,11 +8,14 @@ import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.CommitCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.InitCommand;
 import org.eclipse.jgit.api.PushCommand;
 import org.eclipse.jgit.api.RemoteAddCommand;
 import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.StatusCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
@@ -37,8 +40,13 @@ public class jgitStarter {
 		//File objFileDir = new File("C:\\HIS-Workspace\\1fgl\\repo\\EclipseOxygen\\HIS_QISSERVER_FGL");
 		
 		//Auf Ermanarich, der HISinOne Tomcat
-		File objFileDir = new File("C:\\repo\\Eclipse202312\\HIS_QISSERVER_FGL");
+		//File objFileDir = new File("C:\\repo\\Eclipse202312\\HIS_QISSERVER_FGL");
 		
+		//Zur Entwicklung (auf DEV04), ein Dummy Verzeichnis
+		File objFileDir = new File("C:\\1fgl\\repo\\EclipseOxygen_V01\\Projekt_Kernel02_JAZDummy"); 
+		//File objFileDir = new File("C:\\1fgl\\repo\\EclipseOxygen_V01\\Projekt_Kernel02_JAZDummy\\JAZDummy");
+
+  		
 		
 		//Trotz Einbinden von  in pom.xml Fehlermeldung;
 		//ERROR StatusLogger Log4j2 could not find a logging implementation. Please add log4j-core to the classpath. Using SimpleLogger to log to the console
@@ -50,26 +58,39 @@ public class jgitStarter {
 		//Logger log = LogManager.getLogger(this.getClass().getName());		
 		Logger log = LogManager.getLogger();
 		
-		Git git = Git.init().setDirectory(objFileDir).call();
+		InitCommand gitCommandInit = Git.init();
+		gitCommandInit.setDirectory(objFileDir);
+		Git git = gitCommandInit.call(); //Merke: damit das funktioniert muss der Pfad zu git.exe in der PATH Umgebungsvariablen sein. Z.B. c:\Progamme\Git\bin
 		System.out.println("Git-Repository init done.");
+						
+		CredentialsProvider credentialsProvider = this.createCredentialsProviderByToken(git);
+		System.out.println("Git Credentials Provider created done.");
 		
-		System.out.println("STATUS BEFORE COMMIT");
+		System.out.println("STATUS BEFORE COMMIT");		
 		this.printStatus(git);
         //##################################################################
         
 		//Fuege geänderte Dateien, die schon im Repository sind, hinzu.
 		this.addFileTrackedChanged(git);
 								
-        //Mache einen commit               
-        git.commit().setMessage("Commit by Java-Class a Projekt_Tool_DevEditor Solution").call();
+        //Mache einen commit       
+		CommitCommand gitCommandCommit = git.commit();
+		gitCommandCommit.setMessage("Commit by Java-Class a Projekt_Tool_DevEditor Solution");
+		gitCommandCommit.call();
         //System.out.println("Committed file " + myFile + " to repository at " + git.getRepository().getDirectory());
         
+        System.out.println("STATUS AFTER COMMIT");
+        this.printStatus(git);
+		
+		
+		
         //Mache den push
         //TODOGOON20230310;
         //... wie ??? 
-        this.pushit(git);
+		String sRepoRemote = "https://github.com/firak01/HIS_QISSERVER_FGL.git"; //Noch ungenutzt, muss aufgeteilt werden und dann der PAT Token eingebaut werden.
+        this.pushit(git, credentialsProvider, sRepoRemote);
         
-        System.out.println("STATUS AFTER COMMIT");
+        System.out.println("STATUS AFTER PUSH");
         this.printStatus(git);
         //###############################################################
         
@@ -82,17 +103,30 @@ public class jgitStarter {
 	
 	
 	public void addFileTrackedChanged(Git git) throws NoWorkTreeException, GitAPIException {
-		Status status = git.status().call();
+		
+		StatusCommand gitCommandStatus = git.status();
+		Status status = gitCommandStatus.call();
 
 		Set<String> uncommittedChanges = status.getUncommittedChanges();
+		Set<String> untracked          = status.getUntracked();
 		ArrayList<String> listasUncommitedChanges = new ArrayList<String>();
+		
+		AddCommand gitCommandAdd = git.add();		
         for (String uncommitted : uncommittedChanges) {
-           listasUncommitedChanges.add(uncommitted);
+        	if(!untracked.contains(uncommitted)) {
+        		listasUncommitedChanges.add(uncommitted);
+        	}
         }
         
         // run the add-call 
-        for(String uncommited : listasUncommitedChanges) {
-        	git.add().addFilepattern(uncommited).call();
+        for(String uncommitted : listasUncommitedChanges) {
+        	System.out.println("uncommitted to add: '" + uncommitted + "'");
+        	try {
+        		gitCommandAdd.addFilepattern(uncommitted);
+        		gitCommandAdd.call();
+        	}catch(java.lang.IllegalStateException isex) {
+        		System.out.println(isex.getMessage());
+        	}
         }
        
 	}
@@ -112,76 +146,67 @@ public class jgitStarter {
 	
 	}
 	
-	public void pushit(Git gitPrevious) throws URISyntaxException, InvalidRemoteException, TransportException, GitAPIException {
+	public CredentialsProvider createCredentialsProviderByToken(Git git) {
 		//aus Eclipse-Push Konfiguration:
-		//entspricht dem Github - Projekt - SSH
-		//git@github.com:firak01/HIS_QISSERVER_FGL.git
-		
-		//aus Github - Projekt - HTTPS
-		//https://github.com/firak01/HIS_QISSERVER_FGL.git
-		
-		//##################
-		//Authentifizierung mit https
-		/*https://medium.com/autotrader-engineering/working-with-git-in-java-part-1-a-jgit-tutorial-bc03b404a517
-		Authenticating with a remote
-		Most remote repos will require authentication (at least for the push command). In this tutorial, we’ll be working with remote repositories hosted on GitHub, which has two common authentication methods:
-    	Using a personal access token (PAT) for authentication over HTTPS
-    	Using SSH keys for authentication over SSH
-		To keep things simple in this tutorial, we’ll only be covering HTTPS authentication; SSH is more complex and will be covered in part 2 of this two-part blog post.
+				//entspricht dem Github - Projekt - SSH
+				//git@github.com:firak01/HIS_QISSERVER_FGL.git
+				
+				//aus Github - Projekt - HTTPS
+				//https://github.com/firak01/HIS_QISSERVER_FGL.git
+				
+				//##################
+				//Authentifizierung mit https
+				/*https://medium.com/autotrader-engineering/working-with-git-in-java-part-1-a-jgit-tutorial-bc03b404a517
+				Authenticating with a remote
+				Most remote repos will require authentication (at least for the push command). In this tutorial, we’ll be working with remote repositories hosted on GitHub, which has two common authentication methods:
+		    	Using a personal access token (PAT) for authentication over HTTPS
+		    	Using SSH keys for authentication over SSH
+				To keep things simple in this tutorial, we’ll only be covering HTTPS authentication; SSH is more complex and will be covered in part 2 of this two-part blog post.
 
-		So in the following examples, we’ll be using a personal access token (PAT) for authentication via HTTPS. For more information on creating a PAT token, see the GitHub docs.
-		Providing Credentials for Authentication
+				So in the following examples, we’ll be using a personal access token (PAT) for authentication via HTTPS. For more information on creating a PAT token, see the GitHub docs.
+				Providing Credentials for Authentication
 
-		The JGit command objects for operations such as git push, git pull, and git clone all share a setCredentialsProvider method that allows us to provide credentials to authenticate with the remote repository.
+				The JGit command objects for operations such as git push, git pull, and git clone all share a setCredentialsProvider method that allows us to provide credentials to authenticate with the remote repository.
 
-		The setCredentialsProvider method takes a CredentialsProvider instance as its parameter. This interface has many implementations, the one we need to use for a PAT token is the UsernamePasswordCredentialsProvider (more commonly used for basic authentication).
-		Constructing a CredentialsProvider for a PAT token
+				The setCredentialsProvider method takes a CredentialsProvider instance as its parameter. This interface has many implementations, the one we need to use for a PAT token is the UsernamePasswordCredentialsProvider (more commonly used for basic authentication).
+				Constructing a CredentialsProvider for a PAT token
 
-		The UsernamePasswordCredentialsProvider 's constructor requires a username and password. When using a PAT token, we pass the token as the username and an empty string as the password:
-		 */
-		
-		//Zugang per ACCESS TOKEN ( PAT ) in github: Account, ganz unten im Navigator "Developer Settings"
-		//String sPAT = "nicht hier, schau woanders nach";
-		String sPAT = "";
-		CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(sPAT, ""); //irgendwie empfohlen
-		//CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider("firak01", sPAT); //so funktioniert es auch nicht
-		
-		/*Fehler:
-		 Exception in thread "main" org.eclipse.jgit.errors.UnsupportedCredentialItem: ssh://git@github.com:22: org.eclipse.jgit.transport.CredentialItem$YesNoType:The authenticity of host 'github.com' can't be established.
-RSA key fingerprint is.... .
-Are you sure you want to continue connecting?
-	at org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider.get(UsernamePasswordCredentialsProvider.java:119)
-		 */
+				The UsernamePasswordCredentialsProvider 's constructor requires a username and password. When using a PAT token, we pass the token as the username and an empty string as the password:
+				 */
+				
+				//Zugang per ACCESS TOKEN ( PAT ) in github: Account, ganz unten im Navigator "Developer Settings"
+				//String sPAT = "nicht hier, schau woanders nach";
+				CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(sPAT, ""); //irgendwie empfohlen
+				//CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider("firak01", sPAT); //so funktioniert es auch nicht
+				
+				/*Fehler:
+				 Exception in thread "main" org.eclipse.jgit.errors.UnsupportedCredentialItem: ssh://git@github.com:22: org.eclipse.jgit.transport.CredentialItem$YesNoType:The authenticity of host 'github.com' can't be established.
+		RSA key fingerprint is.... .
+		Are you sure you want to continue connecting?
+			at org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider.get(UsernamePasswordCredentialsProvider.java:119)
+				 */
 
-	  //Daher mal ein neues git Objekt erstellen   
-      //Zwei verschiedene lokale Repos, je nachdem welches Eclipse
-  		//A) auf TUBAF - HISinOne Eclipse:
-  		//File objFileDir = new File("C:\\HIS-Workspace\\1fgl\\repo\\Eclipse202312\\HIS_QISSERVER_FGL");
-  		//B) auf TUBAF (Oxygen Version) für Z-Kernel Entwicklung
-  		//File objFileDir = new File("C:\\HIS-Workspace\\1fgl\\repo\\EclipseOxygen\\HIS_QISSERVER_FGL");
-  		
-  		//Auf Ermanarich, der HISinOne Tomcat
-  		File objFileDir = new File("C:\\repo\\Eclipse202312\\HIS_QISSERVER_FGL");
-  		
-        
-		
-		Git git = Git.init().setDirectory(objFileDir).call();
-		System.out.println("Git-Repository init done.");
-		
-		//aus: https://stackoverflow.com/questions/49292535/how-to-do-gitpush-using-jgit-api
-		// add remote repo:
-//		RemoteAddCommand remoteAddCommand = git.remoteAdd();
-//	    remoteAddCommand.setName("origin");	
-//		remoteAddCommand.setUri(new URIish("https://github.com/firak01/HIS_QISSERVER_FGL.git"));
-		
+			 
+		return credentialsProvider;
+	}
+	
+	public void pushit(Git git, CredentialsProvider credentialsProvider, String sRepoRemote) throws URISyntaxException, InvalidRemoteException, TransportException, GitAPIException {
+				
 		// aber mal explizit als pushCommand
 		PushCommand pushCommand = git.push();
 		//pushCommand.setRemote("https://github.com/firak01/HIS_QISSERVER_FGL.git");
 		//aber Fehler.
-		
+				
 		//An einigen Stellen wird die Syntax der URL mit Username:Token genannt.
 		//git clone https://scuzzlebuzzle:<MYTOKEN>@github.com/scuzzlebuzzle/ol3-1.git --branch=gh-pages gh-pages
-		pushCommand.setRemote("https://firak01:" + sPAT + "@github.com/firak01/HIS_QISSERVER_FGL.git");
+		//pushCommand.setRemote("https://firak01:" + sPAT + "@github.com/firak01/HIS_QISSERVER_FGL.git");
+		
+		//Zur Entwicklung, ein Dummy Projekt
+		//https://github.com/firak01/Projekt_Kernel02_JAZDummy.git
+		
+		//Zugang per ACCESS TOKEN ( PAT ) in github: Account, ganz unten im Navigator "Developer Settings"
+		//String sPAT = "nicht hier, schau woanders nach";
+		pushCommand.setRemote("https://firak01:" + sPAT + "@github.com/firak01/Projekt_Kernel02_JAZDummy.git");
 		
 		//wg Fehler: Caused by: javax.net.ssl.SSLException: Received fatal alert: protocol_version
 		//GitHub verlangt TLS 1.2
@@ -193,51 +218,6 @@ Are you sure you want to continue connecting?
 		// push to remote:
 		pushCommand.setCredentialsProvider(credentialsProvider);
 		pushCommand.call();
-		
-		
-		//############### andere lösung aus stackoverview, aber wohl zu kompliziert
-			//Config config = new Config();
-
-			
-						
-			
-			//Beispielcode:
-			//config.setString("remote", "origin", "pushurl", "short:project.git");			
-			//config.setString("url", "https://server/repos/", "name", "short:");
-			
-			//config.setString("remote", "origin", "pushurl", "short:project.git");
-			//config.setString("url", "git@github.com:firak01/HIS_QISSERVER_FGL.git", "name", "short:");
-			
-			//RemoteConfig rc = new RemoteConfig(config, "origin");
-			
-			
-			//mein Versuch
-			//config.setString("remote", "origin", "pushurl", "short:project.git");
-			
-		  //assertFalse(rc.getPushURIs().isEmpty());
-		  //assertEquals("short:project.git", rc.getPushURIs().get(0).toASCIIString());
-			
-			/*
-			 String trackingBranch = "refs/remotes/" + remote + "/master";
-			  RefUpdate trackingBranchRefUpdate = db.updateRef(trackingBranch);
-			  trackingBranchRefUpdate.setNewObjectId(commit1.getId());
-			  trackingBranchRefUpdate.update();
-
-			  URIish uri = new URIish(db2.getDirectory().toURI().toURL());
-			  remoteConfig.addURI(uri);
-			  remoteConfig.addFetchRefSpec(new RefSpec("+refs/heads/*:refs/remotes/"
-			      + remote + "/*"));
-			  remoteConfig.update(config);
-			  config.save();
-
-
-			  RevCommit commit2 = git.commit().setMessage("Commit to push").call();
-
-			  RefSpec spec = new RefSpec(branch + ":" + branch);
-			  Iterable<PushResult> resultIterable = git.push().setRemote(remote)
-			      .setRefSpecs(spec).call();
-			
-			*/
 		
 	}
 	
