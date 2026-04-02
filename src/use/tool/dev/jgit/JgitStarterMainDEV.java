69559e7c77a586610ed5bf660110711304d77a7d
fl86kyvo <fl86kyvo@URZ-VWIT0-M011.adds.tu-freiberg.de>
2026-04-02 12:02:39 +0200

    Versuch die Launch-Konfiguration auf mehrere Maschinen zu verteilen

--- src/use/tool/dev/jgit/JgitStarterMainDEV.java
+++ src/use/tool/dev/jgit/JgitStarterMainDEV.java
@@ -6,6 +6,7 @@ public class JgitStarterMainDEV {
 	public static void main(String[] args) {
 		
 		//An die Methode des konkreten Projekts durchreichen
+		System.out.println(System.getenv("sPATZZZ"));
 		JgitStarterMain.main(args);
 	}
 }
