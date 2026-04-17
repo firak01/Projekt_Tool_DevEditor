422d01b628203ab0b2745ed6fa2324aecc735804
firak01 <firak01@t-online.de>
2026-04-17 12:19:36 +0200

    Code Kosmetik

--- src/use/tool/dev/jgit/JgitStarterMainDEV.java
+++ src/use/tool/dev/jgit/JgitStarterMainDEV.java
@@ -4,16 +4,6 @@ import use.tool.jgit.JgitStarterMain;
 
 public class JgitStarterMainDEV {
 	public static void main(String[] args) {
-		
-		//Umgebungsvariablen an die Methode des konkreten Projekts durchreichen
-		//Sie sind pro Maschine/Eclipse Instanz ggfs. unterschiedlich
-		//Nicht vergessen: Diese Umgebungsvariablen werden NUR beim Eclipsestart(!) im entsprechenden Starter gesetzt.
-		System.out.println("Vorhandene Umgebungsvariablen, seit Eclipsestart:");
-		System.out.println(System.getenv("MY_TRUSTSTORE"));
-		System.out.println(System.getenv("sPATZZZ"));
-		System.out.println(System.getenv("sRLZZZ"));
-		System.out.println(System.getenv("sRRHZZZ"));		
-		System.out.println(System.getenv("sRRACZZZ"));
 		JgitStarterMain.main(args);
 	}
 }
